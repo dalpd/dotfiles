115e3a4d87fbcec33f2f00184a01c3180c425987
Deniz Alp <denizalpd@ogr.iu.edu.tr>
2021-08-23 22:39:30 +0300

    Initialize new branch for the desktop and add configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -3,42 +3,36 @@
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
 { config, pkgs, ... }:
-let unstable =
-    import (builtins.fetchTarball https://github.com/NixOS/nixpkgs/archive/0747387223edf1aa5beaedf48983471315d95e16.tar.gz)
-    # reuse the current configuration
-    { config = config.nixpkgs.config; };
-in
-{
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+# Pinned at August 8, 2021
+# https://github.com/NixOS/nixpkgs/commit/439b1605227b8adb1357b55ce8529d541abbe9eb
+let
+  unstable = import (builtins.fetchTarball
+    "https://github.com/NixOS/nixpkgs/archive/439b1605227b8adb1357b55ce8529d541abbe9eb.tar.gz") {
+      config = config.nixpkgs.config;
+    };
+in {
+  imports = [ # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+  ];
 
-  # Use the GRUB 2 boot loader.
-  boot.loader.grub.enable = true;
-  boot.loader.grub.version = 2;
-  # boot.loader.grub.efiSupport = true;
-  # boot.loader.grub.efiInstallAsRemovable = true;
-  # boot.loader.efi.efiSysMountPoint = "/boot/efi";
-  # Define on which hard drive you want to install Grub.
-  boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
 
-  networking.hostName = "paxos"; # Define your hostname.
+  networking.hostName = "yakushima"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Set your time zone.
   time.timeZone = "Europe/Istanbul";
 
+  nixpkgs.config.allowUnfree = true;
+
   # The global useDHCP flag is deprecated, therefore explicitly set to false here.
   # Per-interface useDHCP will be mandatory in the future, so this generated config
   # replicates the default behaviour.
   networking.useDHCP = false;
-  networking.interfaces.enp0s31f6.useDHCP = true;
-  networking.interfaces.wlp1s0.useDHCP = true;
-  networking.networkmanager.enable = true;
-  nixpkgs.config.allowUnfree = true;
-
-  nix.useSandbox = true;
+  networking.interfaces.enp3s0.useDHCP = true;
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
@@ -53,23 +47,21 @@ in
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
-  
-  # Enable the Plasma 5 Desktop Environment.
-  services.xserver.displayManager.sddm.enable = true;
-  # services.xserver.desktopManager.plasma5.enable = true;
+  #services.xserver.displayManager.sddm.enable = true;
+  #services.xserver.desktopManager.plasma5.enable = true;
 
-  # Enable XMonad 
+  # Enable XMonad
   services.xserver = {
     windowManager.xmonad = {
       enable = true;
       enableContribAndExtras = true;
       extraPackages = haskellPackages: [
-	haskellPackages.xmonad
+        haskellPackages.xmonad
         haskellPackages.xmonad-contrib
         haskellPackages.xmonad-extras
-	];
-      };
+      ];
     };
+  };
 
   # Configure keymap in X11
   # services.xserver.layout = "us";
@@ -82,51 +74,77 @@ in
   sound.enable = true;
   hardware.pulseaudio.enable = true;
 
+  # Enable bluetooth
+  hardware.bluetooth.enable = true;
+  services.blueman.enable = true;
+
   # Enable touchpad support (enabled default in most desktopManager).
-  services.xserver.libinput.enable = true;
+  # services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.dad = {
     isNormalUser = true;
-    extraGroups = [ "wheel" "video" "docker"]; # Enable ‘sudo’ for the user.
+    extraGroups = [ "wheel" "video" ]; # Enable ‘sudo’ for the user.
+  };
+
+  fonts.fonts = with pkgs; [
+    font-awesome
+    iosevka
+    (nerdfonts.override { fonts = [ "Iosevka" ]; })
+  ];
+
+  fonts.fontconfig.defaultFonts = {
+    monospace = [ "Iosevka" ];
+    sansSerif = [ "Iosevka" ];
+    serif = [ "Iosevka" ];
   };
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    wget
+    git
     alacritty
-    chromium curl unstable.emacs
+    chromium
+    curl
+    emacs
     dolphin
     okular
 
     nixos-icons
+
     breeze-icons
+    oxygen-icons5
+
     libsForQt5.breeze-icons
-    kdeFrameworks.breeze-icons
-    kdeFrameworks.oxygen-icons5
+    libsForQt5.oxygen-icons5
 
-    kdeApplications.kio-extras
-    kdeApplications.kdegraphics-thumbnailers
+    libsForQt5.kio-extras
+    libsForQt5.kdegraphics-thumbnailers
 
-    dmenu networkmanager networkmanager_dmenu
-    unstable.firefox-devedition-bin feh
-    git gimp gwenview
-    # haskellPackages.ghc
-    unstable.haskellPackages.ghc
-    unstable.haskellPackages.zlib
-    # broken in both 20.09 and unstable
-    # haskellPackages.libpq
-    unstable.tdesktop
+    dmenu
+    networkmanager
+    networkmanager_dmenu
+    unstable.firefox-devedition-bin
+    feh
+    git
+    gimp
+    gwenview
+    haskellPackages.ghc
+    haskellPackages.zlib
+    tdesktop
     haskellPackages.postgresql-libpq
     haskellPackages.ghcid
     haskellPackages.cabal-install
-    htop i3lock plasma-pa
+    htop
+    i3lock
+    plasma-pa
     haskellPackages.status-notifier-item
-    spotify scrot wget zlib
+    spotify
+    scrot
+    wget
+    zlib
     python39
-    unstable.texlive.combined.scheme-full
-    font-awesome
-    iosevka
     polybar
     rofi
     unzip
@@ -135,8 +153,6 @@ in
     jq
     irssi
     guvcview
-    #docker
-    #docker-compose
     pavucontrol
     mpv
     gcc
@@ -144,9 +160,10 @@ in
     tree
     ripgrep
     fd
+    pciutils
+    killall
+    nixfmt
   ];
-
-  programs.light.enable = true;
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
@@ -160,10 +177,6 @@ in
 
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
-  services.postgresql = {
-    enable = true;
-    package = pkgs.postgresql_12;
-    };
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
@@ -171,15 +184,13 @@ in
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
-  virtualisation.docker.enable = true;
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "20.09"; # Did you read the comment?
-
+  system.stateVersion = "21.05"; # Did you read the comment?
 
 }
 
